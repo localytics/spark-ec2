0870111dc2d9677005a8a3314a2e87b3741ca22e
ahepburn <ahepburn@localytics.com>
2015-05-19 08:25:48 -0400

    do not set hostname to private dns

--- setup.sh
+++ setup.sh
@@ -23,9 +23,9 @@ source ec2-variables.sh
 # even if the instance is restarted with a different private DNS name
 PRIVATE_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/local-hostname`
 PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/hostname`
-hostname $PRIVATE_DNS
-echo $PRIVATE_DNS > /etc/hostname
-export HOSTNAME=$PRIVATE_DNS  # Fix the bash built-in hostname variable too
+#hostname $PRIVATE_DNS
+#echo $PRIVATE_DNS > /etc/hostname
+#export HOSTNAME=$PRIVATE_DNS  # Fix the bash built-in hostname variable too
 
 echo "Setting up Spark on `hostname`..."
 
