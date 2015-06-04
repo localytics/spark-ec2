d1bfce15bcf2ba4a7e0ed3461d9c767f302bfb8e
Michal Klos <mklos@localytics.com>
2015-06-03 21:21:49 -0400

    Merged our change for allowing a second master with no slaves to setup

--- deploy_templates.py
+++ deploy_templates.py
@@ -19,11 +19,15 @@ master_ram_kb = int(
 # This is the master's memory. Try to find slave's memory as well
 first_slave = os.popen("cat /root/spark-ec2/slaves | head -1").read().strip()
 
-slave_mem_command = "ssh -t -o StrictHostKeyChecking=no %s %s" %\
-        (first_slave, mem_command)
-
-slave_cpu_command = "ssh -t -o StrictHostKeyChecking=no %s %s" %\
-        (first_slave, cpu_command)
+if first_slave:  
+  slave_mem_command = "ssh -t -o StrictHostKeyChecking=no %s %s" %\
+                    (first_slave, mem_command)
+  
+  slave_cpu_command = "ssh -t -o StrictHostKeyChecking=no %s %s" %\
+                    (first_slave, cpu_command)
+else:
+  slave_mem_command = mem_command
+  slave_cpu_command = cpu_command
 
 slave_ram_kb = int(os.popen(slave_mem_command).read().strip())
 
@@ -48,7 +52,7 @@ else:
 
 # Make tachyon_mb as spark_mb for now.
 tachyon_mb = spark_mb
-
+  
 worker_instances = int(os.getenv("SPARK_WORKER_INSTANCES", 1))
 # Distribute equally cpu cores among worker instances
 worker_cores = max(slave_cpus / worker_instances, 1)
@@ -65,11 +69,9 @@ template_vars = {
   "spark_worker_cores": "%d" %  worker_cores,
   "spark_master_opts": os.getenv("SPARK_MASTER_OPTS", ""),
   "spark_version": os.getenv("SPARK_VERSION"),
-  "tachyon_version": os.getenv("TACHYON_VERSION"),
   "hadoop_major_version": os.getenv("HADOOP_MAJOR_VERSION"),
   "java_home": os.getenv("JAVA_HOME"),
   "default_tachyon_mem": "%dMB" % tachyon_mb,
-  "system_ram_mb": "%d" % system_ram_mb,
   "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
   "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
 }
@@ -86,7 +88,7 @@ for path, dirs, files in os.walk(template_dir):
         dest_file = os.path.join(dest_dir, filename)
         with open(os.path.join(path, filename)) as src:
           with open(dest_file, "w") as dest:
-            print("Configuring " + dest_file)
+            print "Configuring " + dest_file
             text = src.read()
             for key in template_vars:
               text = text.replace("{{" + key + "}}", template_vars[key] or '')
